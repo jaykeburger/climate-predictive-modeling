a0858d37aa9f43e0fa0b00801673808cee75dbf7
Tony Flores <tonyflowers756@gmail.com>
2024-02-10 15:22:04 -0600

    base commit for navbar

---
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import sass from './Navbar.sass'
+
+function NavbarItem(props) {
+	return (
+		<div className='NavbarItem'>
+			{props.text}
+		</div>
+	)
+}
+
+function Navbar() {
+  return (
+	  <div className='Navbar'>
+		<NavbarItem text="text"/>
+		<NavbarItem text="other text"/>
+		<NavbarItem text="text"/>
+		<NavbarItem text="other text"/>
+
+	 </div>
+  )
+}
+
+export default Navbar
