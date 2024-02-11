2b4371024c73ba418e5f7effb5577f66a7785cdc
Jake Burger <jaykegamer12@gmail.com>
2024-02-10 19:04:43 -0600

    map svg

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,6 +2,7 @@ import logo from './logo.svg';
 import sass from './App.sass';
 import Navbar from './components/Navbar/Navbar';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import CO2Emissions from "./components/maps/CO2Emissions";
 
 function App() {
 	return (
@@ -9,7 +10,7 @@ function App() {
 			<Router>
 				<Navbar />
 				<Routes>
-					<Route path="/" element={<>path 1</>} />
+					<Route path="/co2-emissions" element={<CO2Emissions/>} />
 					<Route path="/path2" element={<>path 2</>} />
 					<Route path="/path3" element={<>path 3</>} />
 				</Routes>
