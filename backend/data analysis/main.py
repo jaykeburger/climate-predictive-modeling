5b295bb3017de08a47e4e12418bd55b2fe8c2968
allierodrigues <albamaria20013@gmail.com>
2024-02-10 20:31:28 -0600

    python read csv

--- backend/data analysis/main.py
+++ backend/data analysis/main.py
@@ -1,5 +1,7 @@
 import numpy as np
 import pandas as pd
 
-df = pd.read_csv('backend\data\kaggle global-data-on-sustainable-energy.csv')
+df = pd.read_csv('backend\data\co2emissions-per-capita.csv')
+df = df.drop('Code', axis=1)
+
 print(df.info())
